ba27c35040bfa704fd7fb5655155187b2084e577
TejaswiniBeleshe <sonu2201.beleshe@gmail.com>
2024-08-20 14:21:22 +0530

    third commit made

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [ipName,setIpName] = useState('');
-  const [ipPassword,setIpPassword] = useState('');
-  const [check,setCheck] = useState('');
+  const [ipName,setIpName] = useState("");
+  const [ipPassword,setIpPassword] = useState("");
+  const [check,setCheck] = useState("");
 
   const handleSubmit = (e)=>{
     console.log(e.target.value);
@@ -17,7 +17,6 @@ function App() {
     setIpName('');
     setIpPassword('');
   }
-
   return (
     <div className="App">
         <h1>Login Page</h1>
@@ -25,8 +24,8 @@ function App() {
         {check===false && <p>Invalid username or password</p> }
         {!check &&
         <form onSubmit={(e)=>handleSubmit(e)}>
-          <label>Username:<input type="text" placeholder="username" value={ipName} onChange={(e)=>setIpName(e.target.value)} required/></label><br/>
-          <label>Password:<input type="password" placeholder="password" value={ipPassword} onChange={(e)=>setIpPassword(e.target.value)} required/></label><br/>
+          <label>Username:</label><input type="text" placeholder="username" value={ipName} onChange={(e)=>setIpName(e.target.value)} required/><br/>
+          <label>Password:</label><input type="password" placeholder="password" value={ipPassword} onChange={(e)=>setIpPassword(e.target.value)} required/><br/>
           <button type="submit">Submit</button>
         </form>}
     </div>
