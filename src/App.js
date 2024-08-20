f26cc9bf947121d68b0d0d3488c3feaae7999514
TejaswiniBeleshe <sonu2201.beleshe@gmail.com>
2024-08-20 13:14:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ipName,setIpName] = useState('');
+  const [ipPassword,setIpPassword] = useState('');
+  const [check,setCheck] = useState('');
+
+  const handleSubmit = (e)=>{
+    console.log(e.target.value);
+    e.preventDefault();
+    if(ipName === "user" && ipPassword === "password"){
+      setCheck(true);
+    }else{
+      setCheck(false);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Login Page</h1>
+        {check===true && <p>Welcome, user!</p>}
+        {check===false && <p>Invalid username</p> }
+        {!check &&
+        <form onSubmit={(e)=>handleSubmit(e)}>
+          <label>Username:<input type="text" placeholder="username" value={ipName} onChange={(e)=>setIpName(e.target.value)} required/></label><br/>
+          <label>Password:<input type="password" placeholder="password" value={ipPassword} onChange={(e)=>setIpPassword(e.target.value)} required/></label><br/>
+          <button type="submit">Submit</button>
+        </form>}
     </div>
   );
 }
